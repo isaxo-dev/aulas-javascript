5d1b8f8f6eea873de64f7398b614c39021d71788
isaxo-dev <isaxo123321@gmail.com>
2025-07-30 16:40:06 -0300

    Jeitos corretos (e errado) de aspas no texto

--- js/02-meu-script.js
+++ js/02-meu-script.js
@@ -2,6 +2,7 @@
 
 // 1) Crie uma variável chamada "uc" e coloque nela o número da UC que estamos
 
+
 // 2) Crie uma variável chamada "competencia" e coloque nela uma frase que represente o que estamos estudando nesta UC
 
 // 3) Monte e mostre uma frase no console exibindo a UC e a competência. Exemplo: Estamos na UC2 estudando Front-end e JavaScript
@@ -10,3 +11,17 @@ let uc = 2
 let competencia = ("construir aplicações Front-end")
 
 console.log(`No curso de Desenvolvedor Full-stack, no Senac, estamos na UC ${uc}, aprendendo a ${competencia}.`);
+
+//  ERRADO!
+// console.log("Gosto "muito" de paçoca!");
+
+// CORRETO! Aspas simples como comando, e duplas como texto
+console.log('Gosto "muito" de paçoca!');
+
+// CORRETO! Aspas duplas como comando, e simples como texto
+console.log("Gosto 'muito' de paçoca!");
+
+// CORRETO! Escapando de aspas como texto
+console.log("Gosto \"muito\" de paçoca!");
+
+
