f1fbbcc3cb062253b409458c219d1f2761aa9414
isaxo-dev <isaxo123321@gmail.com>
2025-08-06 14:14:39 -0300

    Exemplo de uma array em uso

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -1,3 +1,19 @@
 'use strict';
 
+// Declarar uma array (vetor)
+let vocalistas = ["Ozzy", "Dio", "Lemmy", "David Gilmour"];
+
+// Se quisermos visualizar a estrutura do array inteiro:
+// console.log(vocalistas);
+
+// Acessando um determinado elemento através do índice 
+console.log(vocalistas[0]);
+
+// O David Gilmour está em turnê tocando Pink Floyd
+console.log(`O ${vocalistas[3]} está em turnê tocando Pink Floyd`);
+
+
+
+
+
 
