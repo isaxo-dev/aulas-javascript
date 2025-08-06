167953496402f34ac43a311199eae6e1fd29fdf1
isaxo-dev <isaxo123321@gmail.com>
2025-08-06 15:08:47 -0300

    exercício de array

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -20,6 +20,12 @@ const coisas = [10, "Senac", "<h2>Oie</h2>", seila, 15.88];
 console.log(coisas[1]);
 console.log(coisas[4]);
 
+/* Exercícios (FAÇA AQUI MESMO  ) */
 
+// 1) Crie um array contendo o nome de 7 coisas que você gosta (exemplos: artistas, musicas, livros, comidas... sei lá, inventa!)
 
+// 2) Em seguida, mostre no console uma frase personalizada indicando o segundo, o quinto e o sétimo elemento do array. Use concatenação e/ou template string.
 
+const coisas_que_gosto = ["videogame", "Bistecone", "Música 'Blue', do Young Kai", "skate", "futebol", "dormir", "Fifa"]
+
+console.log(`Normalmente curto assistir Lives do streamer ${coisas_que_gosto[1]} nas noites, porém, quando tem algum jogo de ${coisas_que_gosto[4]} passando no mesmo horário, aí não tem jeito, tenho que assistir... falando nisso, de vez em quando támbem gosto de passar uma raivinha jogando um ${coisas_que_gosto[6]} (kkkk)`);
