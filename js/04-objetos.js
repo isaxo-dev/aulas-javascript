427eb43e5ed6f802c80e8ff116e6270d80e919e6
isaxo-dev <isaxo123321@gmail.com>
2025-08-06 16:31:41 -0300

    Exemplo de objetos na prática

---
+++ js/04-objetos.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Exemplo 1: objeto com dados de uma pessoa
+const pessoa = {
+    // Propriedade: valor
+    nome: "Chaves",
+    idade: 8,
+    cidade: "São Paulo",
+    estado: "SP"
+};  
+
+console.log(pessoa);
+
+
+console.log(`O ${pessoa.nome} tem ${pessoa.idade} anos.`);
+
