0419b63fb15a0450d5caed25f08f122f66b409ca
isaxo-dev <isaxo123321@gmail.com>
2025-08-19 14:47:16 -0300

    Exemplo de condicional composta

--- js/05-condicionais.js
+++ js/05-condicionais.js
@@ -14,4 +14,16 @@ if(numero >= 10){
     console.log("SE você está vendo essa mensagem é porque a condição é verdadeira");
 }
 
+console.log("\nExemplo 2\n");
 
+let aluno = "Ozzy";
+let idade = 30;
+
+/* Lógica: Verificar se o aluno é maior ou menor de idade */
+
+// Condicional COMPOSTA (usa o if e else)
+if(idade < 18){
+    console.log(`${aluno} é menor de idade.`);    
+} else{
+    console.log(`${aluno} é maior de idade!`);
+}
