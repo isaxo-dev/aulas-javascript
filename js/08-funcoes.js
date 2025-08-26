bd4e2a9b4c5244783b0c8668615f9602a0eae740
isaxo-dev <isaxo123321@gmail.com>
2025-08-26 14:19:46 -0300

    Arquivos sobre funções criados

---
+++ js/08-funcoes.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/* Sobre Funções
+Funções são blocos de código responsáveis por executar determinadas tarefas. Para usar estes blocos/funções, é necessárip "chamar/invocar" a função desejada através do seu nome.
+
+Vantagens ao usar funções:
+- Reutilização de código
+- Organização de código-fonte, permitindo a separação de diferentes lógicas e responsabilidades
+- Performance melhor
+- Maior facilidade para manutenções */
