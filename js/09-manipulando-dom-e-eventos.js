41682418a57d97275aacda7e1861ce660a722d52
isaxo-dev <isaxo123321@gmail.com>
2025-08-26 17:26:07 -0300

    Manipulando a página com dom

---
+++ js/09-manipulando-dom-e-eventos.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/* Exemplo 01 */
+
+// Procurando (querySelector) por um elemento chamado exemplo1
+const exemplo01 = document.querySelector("#exemplo01");
+const mensagem01 = document.querySelector("#mensagem01");
+const pagina = document.querySelector("body");
+
+console.log(exemplo01);
+console.log(mensagem01);
+console.log(pagina);
