41a0c2efe7d563a31b17922ec82d667f9a4b71c2
isaxo-dev <isaxo123321@gmail.com>
2025-08-26 14:49:44 -0300

    Exemplos de diferentes tipos de funções

--- js/08-funcoes.js
+++ js/08-funcoes.js
@@ -7,4 +7,43 @@ Vantagens ao usar funções:
 - Reutilização de código
 - Organização de código-fonte, permitindo a separação de diferentes lógicas e responsabilidades
 - Performance melhor
-- Maior facilidade para manutenções */
+- Maior facilidade para manutenções 
+
+No JavaScript, existem várias maneiras de se criar uma função: função anônima, nomeada/declarada e arrow function.*/
+
+console.log("Exemplo 1: Função Anônima");
+const exemplo1 = function(){
+    // corpo da função: ação que a função vai fazer
+    console.log("Olá Função Anônima!");
+};
+
+// Chamando/invocando a função
+exemplo1();
+
+console.log("\nExemplo 2: Função nomeada/declarada");
+function exemplo2(){
+    console.log("Olá função nomeada!");    
+}
+
+exemplo2();
+
+console.log("\nExemplo 3: Arrow Function (Função Flecha/Seta");
+
+/* Sintaxe potencialmente mais simples para
+funções no JavaScript */
+const exemplo3 = () => {
+    console.log("Olá Sintaxe Arrow Function!");
+};
+
+exemplo3();
+
+/* Obs: funções (em qualquer sintaxe) também podem trabalhar com parâmetros/argumentos. 
+
+Quando uma função precisa de valores/dados para algum tipo de processamento, ela recebe valores/dados através de parâmetros/argumentos entre os parênteses.
+
+Geralmente, ao terminar o processamento dos dados, a função "retorna" para fora um resultado*/
+
+
+
+
+
