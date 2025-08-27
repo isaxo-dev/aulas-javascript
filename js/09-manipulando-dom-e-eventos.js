fea7f3f4c407de5f7e91118c9b316e16c9c109e5
isaxo-dev <isaxo123321@gmail.com>
2025-08-27 14:25:29 -0300

    Exemplos de eventos de click e double click

--- js/09-manipulando-dom-e-eventos.js
+++ js/09-manipulando-dom-e-eventos.js
@@ -10,3 +10,14 @@ const pagina = document.querySelector("body");
 console.log(exemplo01);
 console.log(mensagem01);
 console.log(pagina);
+
+// Exemplo detectando eventos
+// Ao clicar 1x, adiciona um texto ao paragrafo de mensagem
+exemplo01.addEventListener("click", function(){
+    mensagem01.textContent = "Evento aqui!"
+});
+
+// Ao clicar 2x, retira o texto do paragrafo de mensagem
+mensagem01.addEventListener("dblclick", function(){
+    mensagem01.textContent = "";
+});
